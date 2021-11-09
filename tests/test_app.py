6afa4421f8efc1209446f492b1be5dd097f222f7
Chuks Akibot <juniorakibor@yahoo.com>
2021-11-09 11:43:55 +0100

    simple flask app

---
+++ tests/test_app.py
@@ -0,0 +1,10 @@
+from app import index
+# import sys
+#
+# sys.path.append('/Volumes/Seagate_Haykay/python-github-actions-CI-CD/src/app')
+#
+# import app
+
+
+def test_index():
+    assert index() == "Hello World"
